7d3922420133d9ee1ec05e28d42d8f2dded1d6b4
Mohamed Mansour <hello@mohamedmansour.com>
2012-08-21 22:05:33 -0700

    Convert to Manifest 2 and include some CSP

---
+++ google_plus_widget.js
@@ -0,0 +1,12 @@
+/**
+ * Google Plus Widget Injection
+ */
+window.___gcfg = {lang: 'en'};
+(function() {
+  var po = document.createElement("script");
+  po.type = "text/javascript";
+  po.async = true;
+  po.src = "https://apis.google.com/js/plusone.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(po, s);
+})();
